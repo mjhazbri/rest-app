3b4c5a75981c17c9d38897a45e35ae8099060fef
Hazbri <jalel.hazbri@docapost.fr>
2016-04-25 18:23:14 +0200

    Cutomization et mongo db:

---
+++ src/main/java/fr/blogspot/mjhazbri/services/CustomerRepository.java
@@ -0,0 +1,15 @@
+package fr.blogspot.mjhazbri.services;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import fr.blogspot.mjhazbri.data.Customer;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+	public Customer findByFirstName(String firstName);
+
+	public List<Customer> findByLastName(String lastName);
+
+}
